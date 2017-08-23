db5518431f72bf9c9969cb2fd3959aad288a2f9d
王卓辉10192452 <wang.zhuohui@zte.com.cn>
2017-08-23 17:16:44 +0800

    add header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = 'SPS-5'
+
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
